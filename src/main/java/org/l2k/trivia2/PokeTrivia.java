36c3bcc90b2b681cfd0a3afc9d892797fba51199
Riley Littlefield <rileylittlefield@ymail.com>
2018-04-23 23:48:26 -0400

    initial commit

---
+++ src/main/java/org/l2k/trivia2/PokeTrivia.java
@@ -0,0 +1,13 @@
+package org.l2k.trivia2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeTrivia {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PokeTrivia.class, args);
+	}
+
+}
