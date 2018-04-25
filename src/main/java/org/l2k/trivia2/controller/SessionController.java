a36cb23f2017ddc9ac8bbe943e82f8649bf8e123
Riley Littlefield <rileylittlefield@ymail.com>
2018-04-25 00:15:09 -0400

    adds spring decorators to session controller

--- src/main/java/org/l2k/trivia2/controller/SessionController.java
+++ src/main/java/org/l2k/trivia2/controller/SessionController.java
@@ -1,17 +1,24 @@
 package org.l2k.trivia2.controller;
 
+import static org.l2k.trivia2.constants.ControllerConstants.Paths;
 import org.l2k.trivia2.domain.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class SessionController {
 	
 	private SessionService sessionService;
 
+	@Autowired
 	public SessionController(SessionService sessionService) {
 		this.sessionService = sessionService;
 	}
 
+	@PostMapping(Paths.SESSION)
 	public ResponseEntity<Session> registerSession(Session session) {
 		Session registeredSession = sessionService.registerSession(session);
 		
