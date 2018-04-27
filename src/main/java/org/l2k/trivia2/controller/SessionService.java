051afb44e81d898487d66adb576a6a0e83a80635
Riley Littlefield <rileylittlefield@ymail.com>
2018-04-26 23:25:31 -0400

    adds Spring Annotations to Session Service

--- src/main/java/org/l2k/trivia2/controller/SessionService.java
+++ src/main/java/org/l2k/trivia2/controller/SessionService.java
@@ -3,12 +3,16 @@ package org.l2k.trivia2.controller;
 import org.l2k.trivia2.domain.Session;
 import org.l2k.trivia2.repository.P2PSessionRepository;
 import org.l2k.trivia2.service.DateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SessionService {
 
 	private P2PSessionRepository sessionRepository;
 	private DateService dateService;
-
+	
+	@Autowired
 	public SessionService(P2PSessionRepository sessionRepository, DateService dateService) {
 		this.sessionRepository = sessionRepository;
 		this.dateService = dateService;
