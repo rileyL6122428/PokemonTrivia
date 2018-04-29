628b2fcb033219d184700c68e0b7201e676a2281
Riley Littlefield <rileylittlefield@ymail.com>
2018-04-28 23:51:55 -0400

    implements & passes first unit spec for P2PSessionRepository#createSession

--- src/main/java/org/l2k/trivia2/repository/P2PSessionRepository.java
+++ src/main/java/org/l2k/trivia2/repository/P2PSessionRepository.java
@@ -1,12 +1,32 @@
 package org.l2k.trivia2.repository;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.l2k.trivia2.domain.Session;
 
 public class P2PSessionRepository {
 
+	private SessionExpirationArbiter expirationArbiter;
+	private Set<Session> activeSessions;
+
+	public P2PSessionRepository(SessionExpirationArbiter expirationArbiter, Set<Session> activeSessions) {
+		this.expirationArbiter = expirationArbiter;
+		this.activeSessions = activeSessions;
+	}
+
 	public Session createSession(Session session) {
-		// TODO Auto-generated method stub
+		activeSessions = activeSessions.stream()
+			.filter((activeSession) -> !expirationArbiter.isExpired(activeSession))
+			.collect(Collectors.toCollection(HashSet::new));
 		return null;
+	}
+
+	public Set<Session> getActiveSessions() {
+		return activeSessions
+				.stream()
+				.collect(Collectors.toCollection(HashSet::new));
 	}
 
 }
---
+++ src/test/java/org/l2k/trivia2/repository/P2PSessionRepositoryTest.java
@@ -0,0 +1,82 @@
+package org.l2k.trivia2.repository;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.l2k.trivia2.domain.Session;
+import org.mockito.Mock;
+
+import name.falgout.jeffrey.testing.junit5.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class P2PSessionRepositoryTest {
+
+	private P2PSessionRepository sessionRepository;
+	
+	@Mock private SessionExpirationArbiter expirationArbiter;
+	@Mock private Session preExistingSession1;
+	@Mock private Session preExistingSession2;
+	
+	@BeforeEach
+	void setup() {
+		sessionRepository = new P2PSessionRepository(expirationArbiter, new HashSet<Session>() {{
+			add(preExistingSession1);
+			add(preExistingSession2);
+		}}) ;
+	}
+	
+	@Nested
+	class CreateSession {
+		
+		@Mock private Session newSession; 
+		
+		@Test
+		void clearsUnSyncedSessions() {
+			when(expirationArbiter.isExpired(preExistingSession1)).thenReturn(true);
+			when(expirationArbiter.isExpired(preExistingSession2)).thenReturn(false);
+			
+			sessionRepository.createSession(newSession);
+			
+			Set<Session> activeSessions = sessionRepository.getActiveSessions();
+			assertFalse(activeSessions.contains(preExistingSession1));
+			assertTrue(activeSessions.contains(preExistingSession2));
+		}
+		
+		@Disabled
+		@Test
+		void returnsNullIfNameRepositoryEmpty() {
+			fail("Not yet implemented");
+		}
+		
+		@Disabled
+		@Test
+		void returnsSessionIfNameRepositoryHasVacantNames() {
+			fail("Not yet implemented");
+		}
+		
+		@Disabled
+		@Test
+		void storesSessionAsReadyToBeSynced() {
+			fail("Not yet implemented");
+		}
+	}
+
+	public P2PSessionRepository getSessionRepository() {
+		return sessionRepository;
+	}
+
+	public void setSessionRepository(P2PSessionRepository sessionRepository) {
+		this.sessionRepository = sessionRepository;
+	}
+
+}
