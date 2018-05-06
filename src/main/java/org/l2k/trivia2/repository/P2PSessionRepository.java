545c202cc2658eb798877481d1d52f0beada4a7c
Riley Littlefield <rileylittlefield@ymail.com>
2018-05-05 20:44:52 -0400

    adds spring annotations to SessionRepository

--- src/main/java/org/l2k/trivia2/repository/P2PSessionRepository.java
+++ src/main/java/org/l2k/trivia2/repository/P2PSessionRepository.java
@@ -1,13 +1,17 @@
 package org.l2k.trivia2.repository;
 
 import org.l2k.trivia2.domain.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class P2PSessionRepository {
 
 	private SessionExpirationArbiter expirationArbiter;
 	private NameRepository nameRepository;
 	private SessionTable sessionTable;
 
+	@Autowired
 	public P2PSessionRepository(SessionExpirationArbiter expirationArbiter, SessionTable sessionTable, NameRepository nameRepository) {
 		this.expirationArbiter = expirationArbiter;
 		this.nameRepository = nameRepository;
