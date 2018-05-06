a519543624080052032f9956f8de258334a755f6
Riley Littlefield <rileylittlefield@ymail.com>
2018-05-05 21:14:05 -0400

    adds specs for and implements NameRepository#takeName

--- src/main/java/org/l2k/trivia2/repository/NameRepository.java
+++ src/main/java/org/l2k/trivia2/repository/NameRepository.java
@@ -1,10 +1,21 @@
 package org.l2k.trivia2.repository;
 
+import java.util.LinkedList;
+
 public class NameRepository {
+	
+	private LinkedList<String> names;
+	private boolean takeFromBeginningOfList;
+
+	public NameRepository(LinkedList<String> names) {
+		this.names = names;
+	}
 
 	public String takeName() {
-		// TODO Auto-generated method stub
-		return null;
+		if (names.isEmpty()) { return null; }
+		String nextName = takeFromBeginningOfList ? names.removeFirst() : names.removeLast();
+		takeFromBeginningOfList = !takeFromBeginningOfList;
+		return nextName;			
 	}
 
 }
