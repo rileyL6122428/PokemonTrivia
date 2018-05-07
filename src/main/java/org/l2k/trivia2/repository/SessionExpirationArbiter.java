991889b4dcb1f01d8b3fc6d4c30a78b3c0d56bad
Riley Littlefield <rileylittlefield@ymail.com>
2018-05-06 21:08:05 -0400

    adds spring annotations

--- src/main/java/org/l2k/trivia2/repository/SessionExpirationArbiter.java
+++ src/main/java/org/l2k/trivia2/repository/SessionExpirationArbiter.java
@@ -4,13 +4,18 @@ import java.util.Date;
 
 import org.l2k.trivia2.domain.Session;
 import org.l2k.trivia2.service.DateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SessionExpirationArbiter {
 
 	private long syncThreshold;
 	private DateService dateService;
 
-	public SessionExpirationArbiter(long syncThreshold, DateService dateService) {
+	@Autowired
+	public SessionExpirationArbiter(@Qualifier("EXPIRATION_SYNC_THRESHOLD") long syncThreshold, DateService dateService) {
 		this.syncThreshold = syncThreshold;
 		this.dateService = dateService;
 	}
