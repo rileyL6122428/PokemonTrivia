ffba6ed470858e710e51b3e0f6df69693bd57ae0
Riley Littlefield <rileylittlefield@ymail.com>
2018-05-07 22:48:39 -0400

    adds unit specs & impls for SessionTable#clearRecords

--- src/main/java/org/l2k/trivia2/repository/SessionTable.java
+++ src/main/java/org/l2k/trivia2/repository/SessionTable.java
@@ -1,8 +1,11 @@
 package org.l2k.trivia2.repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.l2k.trivia2.domain.Session;
 
@@ -14,9 +17,12 @@ public class SessionTable {
 		this.sessions = sessions;
 	}
 
-	public void clearRecords(Predicate<Session> predicate) {
-		// TODO Auto-generated method stub
-		
+	public List<Session> clearRecords(Predicate<Session> predicate) {
+		return new ArrayList<Session>(sessions.values())
+			.stream()
+			.filter(predicate)
+			.map((session) -> sessions.remove(session.getId()))
+			.collect(Collectors.toCollection(ArrayList::new));
 	}
 
 	public void saveRecord(Session session) {
