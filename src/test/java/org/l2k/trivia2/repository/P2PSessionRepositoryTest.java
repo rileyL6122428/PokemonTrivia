98a479ce2a7c9dff88cd64c48440ab93ea2e89b0
Riley Littlefield <rileylittlefield@ymail.com>
2018-05-14 01:58:28 -0400

    optimizes imports

--- src/test/java/org/l2k/trivia2/repository/P2PSessionRepositoryTest.java
+++ src/test/java/org/l2k/trivia2/repository/P2PSessionRepositoryTest.java
@@ -3,7 +3,6 @@ package org.l2k.trivia2.repository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -13,7 +12,6 @@ import java.util.Date;
 import java.util.function.Predicate;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
