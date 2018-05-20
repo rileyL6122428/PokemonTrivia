9312b500c2a67df6e2645efd3cf674bc466949e7
Riley Littlefield <rileylittlefield@ymail.com>
2018-05-19 23:13:41 -0400

    adds session service adapter

--- poketrivia-client/src/app/session/session.adapter.ts
+++ poketrivia-client/src/app/session/session.adapter.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Session } from './session.model';
 
 @Injectable()
 export class SessionServiceAdapter {
 
-
+  mapFromPOJO(sessionPOJO: any) {
+    return new Session(
+      sessionPOJO.id,
+      sessionPOJO.name,
+      new Date(sessionPOJO.lastUpdated)
+    );
+  }
 
 }
--- poketrivia-client/src/app/session/session.model.ts
+++ poketrivia-client/src/app/session/session.model.ts
@@ -1,5 +1,7 @@
 export class Session {
   constructor(
-    readonly id: string
+    readonly id: string,
+    readonly name: string,
+    readonly lastUpdate: Date
   ) { }
 }
