5af2de4f9b97ec4b78c975eea8fb1a1a77e84b97
Riley Littlefield <rileylittlefield@ymail.com>
2018-05-22 23:49:39 -0400

    begins test setup for SessionService#openConnections

--- poketrivia-client/src/app/session/session.service.spec.ts
+++ poketrivia-client/src/app/session/session.service.spec.ts
@@ -1,15 +1,75 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+import { TestBed, inject, async } from '@angular/core/testing';
 import { SessionService } from './session.service';
+import { SessionHttp } from './session.http';
+import { SessionServiceAdapter } from './session.adapter';
+import { SessionStomp } from './session.stomp';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
 
 describe('SessionService', () => {
+
+  let sessionService: SessionService;
+  let sessionHttpMock;
+  let sessionAdapterMock;
+  let sessionStompMock;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [SessionService]
+      providers: [
+        SessionService,
+        {
+          provide: SessionHttp,
+          useValue: jasmine.createSpyObj('sessionHttp', ['registerSession'])
+        },
+        {
+          provide: SessionServiceAdapter,
+          useValue: jasmine.createSpyObj('sessionAdapter', ['mapFromPOJO'])
+        },
+        {
+          provide: SessionStomp,
+          useValue: jasmine.createSpyObj('sessionStomp', ['initializeConnection'])
+        }
+      ]
     });
+    // debugger
+    sessionService = TestBed.get(SessionService),
+    sessionHttpMock = TestBed.get(SessionHttp),
+    sessionAdapterMock = TestBed.get(SessionServiceAdapter),
+    sessionStompMock = TestBed.get(SessionStomp);
   });
 
-  it('should be created', inject([SessionService], (service: SessionService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should be created', () => expect(sessionService).toBeTruthy());
+
+  fdescribe('#openConnections', () => {
+
+    let registerSessionObserver: Observer<any>;
+    let unmappedSession: any;
+    let mappedSession: any;
+
+    beforeEach(() => {
+      _stubRegisterSession();
+      _stubMapPOJO();
+    });
+
+    it('calls SessionHttp#registerSession', async(() => {
+      sessionService.openConnections()
+        .subscribe((successful) => {
+          expect(sessionHttpMock.registerSession).toHaveBeenCalled();
+        });
+
+      registerSessionObserver.next(unmappedSession);
+    }));
+
+    function _stubRegisterSession() {
+      sessionHttpMock.registerSession.and.returnValue(
+        new Observable<any>((observer) => registerSessionObserver = observer)
+      );
+      unmappedSession = { id: 'EXAMPLE_ID_FOR_UNMAPPED_SESSION' };
+    }
+
+    function _stubMapPOJO() {
+      mappedSession = { id: 'EXAMPLE_ID_FOR_MAPPED_SESSION' };
+      sessionAdapterMock.mapFromPOJO.and.returnValue(mappedSession);
+    }
+  });
 });
