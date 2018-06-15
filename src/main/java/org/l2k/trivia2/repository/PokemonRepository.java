51f8a97014da4b65428bf133dd51955d83feeb23
Riley Littlefield <rileylittlefield@ymail.com>
2018-06-15 00:34:49 -0400

    adds spring annotations to hook up data providers

--- src/main/java/org/l2k/trivia2/repository/PokemonRepository.java
+++ src/main/java/org/l2k/trivia2/repository/PokemonRepository.java
@@ -3,12 +3,17 @@ package org.l2k.trivia2.repository;
 import java.util.List;
 
 import org.l2k.trivia2.domain.Pokemon;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PokemonRepository {
 
 	private List<Pokemon> pokemon;
 	
-	public PokemonRepository(List<Pokemon> pokemon) {
+	@Autowired
+	public PokemonRepository(@Qualifier("pokemon") List<Pokemon> pokemon) {
 		this.pokemon = pokemon;
 	}
 
