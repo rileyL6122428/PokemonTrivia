f9ab6daecf5f7d7ad96586f5c5bfd7bca326a7d3
Riley Littlefield <rileylittlefield@ymail.com>
2018-06-20 17:43:35 -0400

    adds working impl of PokemonService#fetchPokemon (#31)
    * adds working impl of PokemonService#fetchPokemon
    
    * adds error handling for PokemonService#fetchPokemon
    
    * deletes dead code
    
    * deletes dead whitespace
    
    * fixes stubbed method in PokemonService jamsine spec
    
    * adds pokemon injectable providers

--- poketrivia-client/src/app/pokemon/pokemon.module.ts
+++ poketrivia-client/src/app/pokemon/pokemon.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PokemonHttp } from './pokemon.http';
+import { PokemonStore } from './pokemon.store';
+import { PokemonService } from './pokemon.service';
+import { PokemonAdapter } from './pokemon.adapter';
 
 @NgModule({
   providers: [
-    PokemonHttp
+    PokemonHttp,
+    PokemonAdapter,
+    PokemonStore,
+    PokemonService
   ]
 })
 export class PokemonModule { }
---
+++ poketrivia-client/src/app/pokemon/pokemon.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { PokemonHttp, UnmappedPokemon } from './pokemon.http';
+import { PokemonAdapter } from './pokemon.adapter';
+import { map } from 'rxjs/operators/map';
+import { tap } from 'rxjs/operators/tap';
+import { catchError } from 'rxjs/operators/catchError';
+import { of } from 'rxjs/Observable/of';
+import { Pokemon } from './pokemon.model';
+import { PokemonStore } from './pokemon.store';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class PokemonService {
+
+  constructor(
+    private httpUtil: PokemonHttp,
+    private adapter: PokemonAdapter,
+    private store: PokemonStore
+  ) { }
+
+  fetchPokemon(): Observable<boolean> {
+    return this.httpUtil
+      .fetchAll()
+      .pipe(map(
+        (unmapped: Array<UnmappedPokemon>) => this.adapter.mapPokemons(unmapped)
+      ))
+      .pipe(tap((pokemon: Array<Pokemon>) => {
+        this.store.depositList(pokemon);
+      }))
+      .pipe(map(() => true))
+      .pipe(catchError((() => of(false))));
+  }
+
+  getPokemonByName(name: string): Pokemon {
+    return null;
+  }
+
+}
