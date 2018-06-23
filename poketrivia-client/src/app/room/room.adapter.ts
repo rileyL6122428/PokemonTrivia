dcd3ddfbf8239c21e0fc7c80f4c614f58c1feb10
Riley Littlefield <rileylittlefield@ymail.com>
2018-06-23 02:42:38 -0400

    adds RoomAdapter#mapRoom (#34)

---
+++ poketrivia-client/src/app/room/room.adapter.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { UnmappedRoom } from './room.http';
+import { Room } from './room.model';
+import { PokemonStore } from '../pokemon/pokemon.store';
+
+@Injectable()
+export class RoomAdapter {
+
+  constructor(
+    private pokemonStore: PokemonStore
+  ) { }
+
+  mapRoom(unmapped: UnmappedRoom): Room {
+    return new Room(
+      unmapped.mascotName,
+      () => this.pokemonStore.getByName(unmapped.mascotName)
+    );
+  }
+
+}
--- poketrivia-client/src/app/room/room.model.ts
+++ poketrivia-client/src/app/room/room.model.ts
@@ -3,6 +3,7 @@ import { Pokemon } from '../pokemon/pokemon.model';
 export class Room {
 
   constructor(
+    readonly name: string,
     private mascotGetter: () => Pokemon
   ) { }
 
