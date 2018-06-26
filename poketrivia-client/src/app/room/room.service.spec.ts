afebefa4daca5df004fb2ade8eb1a0f16c42cff6
Riley Littlefield <rileylittlefield@ymail.com>
2018-06-25 22:53:11 -0400

    implements RoomService#fecthAll

--- poketrivia-client/src/app/room/room.service.spec.ts
+++ poketrivia-client/src/app/room/room.service.spec.ts
@@ -5,12 +5,14 @@ import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
 import { Room } from './room.model';
 import { RoomAdapter } from './room.adapter';
+import { RoomStore } from './room.store';
 
 describe('RoomService', () => {
 
   let roomService: RoomService;
   let roomHttpMock: any;
   let roomAdapterMock: any;
+  let roomStoreMock: any;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -23,6 +25,10 @@ describe('RoomService', () => {
         {
           provide: RoomAdapter,
           useValue: jasmine.createSpyObj('roomAdapter', ['mapRooms'])
+        },
+        {
+          provide: RoomStore,
+          useValue: jasmine.createSpyObj('roomStore', ['depositList'])
         }
       ]
     });
@@ -30,11 +36,13 @@ describe('RoomService', () => {
     roomService = TestBed.get(RoomService);
     roomHttpMock = TestBed.get(RoomHttp);
     roomAdapterMock = TestBed.get(RoomAdapter);
+    roomStoreMock = TestBed.get(RoomStore);
   });
 
   describe('#fetchAll', () => {
     let fetchAllObserver: Observer<Array<UnmappedRoom>>;
     let unmappedRooms: Array<UnmappedRoom>;
+    let mappedRooms: Array<Room>;
 
     beforeEach(() => {
       _stubRoomHttp();
@@ -56,6 +64,36 @@ describe('RoomService', () => {
       fetchAllObserver.next(unmappedRooms);
     }));
 
+    it('passes mapped rooms to roomStore for storage', async(() => {
+      roomService
+        .fetchAll()
+        .subscribe(() => {
+          expect(roomStoreMock.depositList).toHaveBeenCalledWith(mappedRooms);
+        });
+
+      fetchAllObserver.next(unmappedRooms);
+    }));
+
+    it('returns true when no errors encountered in process', async(() => {
+      roomService
+        .fetchAll()
+        .subscribe((successful: boolean) => {
+          expect(successful).toBe(true);
+        });
+
+      fetchAllObserver.next(unmappedRooms);
+    }));
+
+    it('returns false when error encountered', async(() => {
+      roomService
+        .fetchAll()
+        .subscribe((successful: boolean) => {
+          expect(successful).toBe(false);
+        });
+
+      fetchAllObserver.error('SERVER ERROR');
+    }));
+
     function _stubRoomHttp() {
       roomHttpMock.getAll.and.returnValue(
         new Observable(observer => fetchAllObserver = observer
@@ -67,6 +105,13 @@ describe('RoomService', () => {
         { mascotName: 'Pikachu' },
         { mascotName: 'Eevee' },
       ];
+
+      mappedRooms = [
+        new Room('Pikachu', null),
+        new Room('Eevee', null)
+      ];
+
+      roomAdapterMock.mapRooms.and.returnValue(mappedRooms);
     }
   });
 });
--- poketrivia-client/src/app/room/room.service.ts
+++ poketrivia-client/src/app/room/room.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
 import { Room } from './room.model';
 import { RoomHttp, UnmappedRoom } from './room.http';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/Observable/of';
 import { map } from 'rxjs/operators/map';
+import { tap } from 'rxjs/operators/tap';
+import { catchError } from 'rxjs/operators/catchError';
 import { RoomAdapter } from './room.adapter';
+import { RoomStore } from './room.store';
 
 @Injectable()
 export class RoomService {
 
   constructor(
     private http: RoomHttp,
-    private adapter: RoomAdapter
+    private adapter: RoomAdapter,
+    private store: RoomStore
   ) { }
 
   fetchAll(): Observable<boolean> {
     return this.http
       .getAll()
       .pipe(map(
-        (unmapped: Array<UnmappedRoom>) => this.adapter.mapRooms(unmapped)
+        (unmappedRooms: Array<UnmappedRoom>) => this.adapter.mapRooms(unmappedRooms)
+      ))
+      .pipe(tap(
+        (mappedRooms: Array<Room>) => this.store.depositList(mappedRooms)
       ))
       .pipe(map(
         () => true
+      ))
+      .pipe(catchError(
+        () => of(false)
       ));
   }
 
