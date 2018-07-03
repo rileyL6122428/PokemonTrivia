f4d7e72f3988e800c2fca96bcd3f27f35c73706d
Riley Littlefield <rileylittlefield@ymail.com>
2018-07-02 23:23:45 -0400

    begins styling reset; redirects to client root when rooms not fetched

--- poketrivia-client/src/app/matchmaking/matchmaking.component.ts
+++ poketrivia-client/src/app/matchmaking/matchmaking.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Room } from '../room/room.model';
 import { MatchmakingService } from './matchmaking.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-matchmaking',
@@ -12,11 +13,21 @@ export class MatchmakingComponent implements OnInit {
   rooms: Array<Room>;
 
   constructor(
-    private matchmakingService: MatchmakingService
+    private matchmakingService: MatchmakingService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.rooms = this.matchmakingService.allRooms();
+    if (this.roomsEmpty) {
+      this.router.navigateByUrl('/');
+    }
+  }
+
+  private get roomsEmpty(): boolean {
+    console.log('!this.rooms', !this.rooms);
+    console.log('!this.rooms.length', !this.rooms.length);
+    return !(this.rooms && this.rooms.length);
   }
 
 }
