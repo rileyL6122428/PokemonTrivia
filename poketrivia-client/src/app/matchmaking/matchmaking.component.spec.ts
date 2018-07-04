11fb9875b50d39ea77a5205886e3786e5c1f41b4
Riley Littlefield <rileylittlefield@ymail.com>
2018-07-04 16:26:39 -0400

    Matchmaking ui (#41)
    * Begins matchmaking ui
    
    * begins styling reset; redirects to client root when rooms not fetched
    
    * adds functionlity to roomService: roomService#fetchAll returns false when empty list returned from server
    
    * begins pokemon button styling
    
    * adds basic room button hover api
    
    * touches up spacing in matchmaking ui stylesheet
    
    * deletes logger statements

--- poketrivia-client/src/app/matchmaking/matchmaking.component.spec.ts
+++ poketrivia-client/src/app/matchmaking/matchmaking.component.spec.ts
@@ -3,6 +3,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatchmakingComponent } from './matchmaking.component';
 import { MatchmakingService } from './matchmaking.service';
 import { Room } from '../room/room.model';
+import { Router } from '@angular/router';
+import { SafeHtmlPipe } from '../html-interpolation/safe.pipe';
+import { Pokemon } from '../pokemon/pokemon.model';
 
 describe('MatchmakingComponent', () => {
   let matchmakingComponent: MatchmakingComponent;
@@ -10,14 +13,19 @@ describe('MatchmakingComponent', () => {
   let fixture: ComponentFixture<MatchmakingComponent>;
   let matchmakingServiceMock: any;
   let rooms: Array<Room>;
+  let routerMock: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MatchmakingComponent ],
+      declarations: [ MatchmakingComponent, SafeHtmlPipe ],
       providers: [
         {
           provide: MatchmakingService,
           useValue: jasmine.createSpyObj('matchmakingService', ['allRooms'])
+        },
+        {
+          provide: Router,
+          useValue: jasmine.createSpyObj('router', ['navigateByUrl'])
         }
       ]
     })
@@ -26,26 +34,35 @@ describe('MatchmakingComponent', () => {
 
   beforeEach(() => {
     _stubMatchmakingService();
-    _affixComponent();
+    routerMock = TestBed.get(Router);
   });
 
   it('should create', () => {
+    _affixComponent();
     expect(matchmakingComponent).toBeTruthy();
   });
 
   describe('#ngOnInit', () => {
     it('gets all rooms fetched from app load', () => {
+      _affixComponent();
       expect(matchmakingServiceMock.allRooms).toHaveBeenCalled();
       expect(matchmakingComponent.rooms).toBe(rooms);
     });
 
     it('adds a room button to the view for each fetched room', () => {
-      const roomButtons: Array<Element> = Array.from(domRoot.querySelectorAll('button.room-button'));
+      _affixComponent();
 
+      const roomButtons: Array<Element> = Array.from(domRoot.querySelectorAll('button.room-button'));
       expect(roomButtons.length).toBe(2);
       expect(roomButtons[0].id).toBe('pikachu-room-button');
       expect(roomButtons[1].id).toBe('eevee-room-button');
     });
+
+    it('redirects back to app load if rooms haven\'t been fetched', async(() => {
+      matchmakingServiceMock.allRooms.and.returnValue([]);
+      _affixComponent();
+      expect(routerMock.navigateByUrl).toHaveBeenCalledWith('/');
+    }));
   });
 
   function _affixComponent() {
@@ -58,8 +75,8 @@ describe('MatchmakingComponent', () => {
   function _stubMatchmakingService() {
     matchmakingServiceMock = TestBed.get(MatchmakingService);
     rooms = [
-      new Room('pikachu', null),
-      new Room('eevee', null),
+      new Room('pikachu', () => new Pokemon('Pikachu', '', '')),
+      new Room('eevee', () => new Pokemon('Eevee', '', '')),
     ];
     matchmakingServiceMock.allRooms.and.returnValue(rooms);
   }
--- poketrivia-client/src/app/matchmaking/matchmaking.component.ts
+++ poketrivia-client/src/app/matchmaking/matchmaking.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Room } from '../room/room.model';
 import { MatchmakingService } from './matchmaking.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-matchmaking',
@@ -12,11 +13,19 @@ export class MatchmakingComponent implements OnInit {
   rooms: Array<Room>;
 
   constructor(
-    private matchmakingService: MatchmakingService
+    private matchmakingService: MatchmakingService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.rooms = this.matchmakingService.allRooms();
+    if (this.roomsEmpty) {
+      this.router.navigateByUrl('/');
+    }
+  }
+
+  private get roomsEmpty(): boolean {
+    return !(this.rooms && this.rooms.length);
   }
 
 }
