d01914eb6595239af910169f586e480fe9640949
Riley Littlefield <rileylittlefield@ymail.com>
2018-07-08 22:48:47 -0400

    adds spring annotations

--- src/main/java/org/l2k/trivia2/controller/RoomController.java
+++ src/main/java/org/l2k/trivia2/controller/RoomController.java
@@ -2,14 +2,19 @@ package org.l2k.trivia2.controller;
 
 import java.util.List;
 
+import org.l2k.trivia2.constants.ControllerConstants.PathVariables;
 import org.l2k.trivia2.constants.ControllerConstants.Paths;
 import org.l2k.trivia2.domain.P2PSession;
 import org.l2k.trivia2.domain.Room;
 import org.l2k.trivia2.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class RoomController {
@@ -26,9 +31,13 @@ public class RoomController {
 		return ResponseEntity.ok(roomService.getRooms());
 	}
 
-	public ResponseEntity<Room> joinRoom(String roomName, P2PSession user) {
+	@PostMapping(Paths.JOIN_ROOM)
+	public ResponseEntity<Room> joinRoom(
+			@PathVariable(PathVariables.ROOM_NAME) String roomName, 
+			@RequestBody P2PSession user
+	) {
 		Room room = roomService.joinRoom(roomName, user);
-		return room != null ? ResponseEntity.ok(room) : ResponseEntity.status(403).body(room);
+		return room != null ? ResponseEntity.ok(room) : ResponseEntity.status(HttpStatus.FORBIDDEN).body(room);
 	}
 
 }
