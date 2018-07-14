6c614f507cdf52636a49b7272832a59c8829b195
Riley Littlefield <rileylittlefield@ymail.com>
2018-07-14 19:48:52 -0400

    removes generated boilerplate from new reusable ui components

--- poketrivia-client/src/app/reusable-ui/error-message/error-message.component.ts
+++ poketrivia-client/src/app/reusable-ui/error-message/error-message.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'pkt-error-message',
   templateUrl: './error-message.component.html',
   styleUrls: ['./error-message.component.scss']
 })
-export class ErrorMessageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class ErrorMessageComponent { }
