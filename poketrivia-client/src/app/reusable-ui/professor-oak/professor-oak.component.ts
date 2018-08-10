b66a86aa05f3e38b7889ccd878f9818867abab35
Riley Littlefield <rileylittlefield@ymail.com>
2018-08-10 00:06:18 -0400

    deletes dead code

--- poketrivia-client/src/app/reusable-ui/professor-oak/professor-oak.component.ts
+++ poketrivia-client/src/app/reusable-ui/professor-oak/professor-oak.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'pkt-professor-oak',
   templateUrl: './professor-oak.component.html',
   styleUrls: ['./professor-oak.component.scss']
 })
-export class ProfessorOakComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class ProfessorOakComponent {
 
 }
