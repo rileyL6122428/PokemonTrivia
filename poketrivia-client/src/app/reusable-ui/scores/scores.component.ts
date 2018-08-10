91730cd9ea8496e67f682d06003851063a9c763c
Riley Littlefield <rileylittlefield@ymail.com>
2018-08-10 00:09:22 -0400

    deletes dead coce

--- poketrivia-client/src/app/reusable-ui/scores/scores.component.ts
+++ poketrivia-client/src/app/reusable-ui/scores/scores.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'pkt-scores',
   templateUrl: './scores.component.html',
   styleUrls: ['./scores.component.scss']
 })
-export class ScoresComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class ScoresComponent {
 
 }
