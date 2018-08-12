4daf947664427eb7eef295b2069b6629aebf09a2
Riley Littlefield <rileylittlefield@ymail.com>
2018-08-12 01:37:27 -0400

    adds pokeball open and close animation (#51)
    * adds pokeball open and close animation
    
    * renames method

--- poketrivia-client/src/app/reusable-ui/poke-ball/poke-ball.component.ts
+++ poketrivia-client/src/app/reusable-ui/poke-ball/poke-ball.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { OpenablePokeball } from './poke-ball.model';
 
 @Component({
   selector: 'pkt-poke-ball',
@@ -8,5 +9,57 @@ import { Component, Input } from '@angular/core';
 export class PokeBallComponent {
 
   @Input('rolling') animateRoll: boolean;
+  @Input() openable: boolean;
+
+  pokeball: OpenablePokeball;
+
+  constructor() {
+    this.pokeball = new OpenablePokeball();
+  }
+
+  toggleOpenClose(): void {
+    if (this.openable) {
+      this.pokeball.toggleOpenClose();
+    }
+  }
+
+  private get topCircleD(): string {
+    return `
+      M
+        5 60
+      C
+        5 -10
+        105 -10
+        105 60
+
+      C
+        105 ${this.pokeball.topOpeningHeight}
+        5 ${this.pokeball.topOpeningHeight}
+        5 60
+    `;
+  }
+
+  private get bottomCircleD(): string {
+    return `
+      M
+        5 60
+      C
+        5 130
+        105 130
+        105 60
+      C
+        105 ${this.pokeball.bottomOpeningHeight}
+        5 ${this.pokeball.bottomOpeningHeight}
+        5 60
+    `;
+  }
+
+  get buttonHeight(): number {
+    return this.pokeball.buttonHeight;
+  }
+
+  get buttonRadiusY(): number {
+    return this.pokeball.buttonRadiusY;
+  }
 
 }
---
+++ poketrivia-client/src/app/reusable-ui/poke-ball/poke-ball.model.ts
@@ -0,0 +1,144 @@
+export class OpenablePokeball {
+
+  private _topOpeningHeight: number;
+  private _bottomOpeningHeight: number;
+  private _buttonHeight: number;
+  private _buttonRadiusY: number;
+  private animatingOpenOrClose: boolean;
+
+  constructor() {
+    this._topOpeningHeight = this.circleBaselineOpeningHeight;
+    this._bottomOpeningHeight = this.circleBaselineOpeningHeight;
+    this._buttonHeight = this.circleBaselineOpeningHeight;
+    this._buttonRadiusY = this.buttonBaselineRadiusY;
+  }
+
+  get topOpeningHeight(): number {
+    return this._topOpeningHeight;
+  }
+
+  get bottomOpeningHeight(): number {
+    return this._bottomOpeningHeight;
+  }
+
+  get buttonHeight(): number {
+    return this._buttonHeight;
+  }
+
+  get buttonRadiusY(): number {
+    return this._buttonRadiusY;
+  }
+
+  toggleOpenClose(): void {
+    if (!this.animatingOpenOrClose && this.isOpen) {
+      this.animateClose();
+    } else if (!this.animatingOpenOrClose && !this.isOpen) {
+      this.animateOpen();
+    }
+  }
+
+  animateOpen(): void {
+    this.animateChangeInOpeningHeight({
+      topHeightIncrement: -this.topCircleAnimationIncrement,
+      finalTopHeight: this.topCircleMaxOpeningHeight,
+      bottomHeightIncrement: this.bottomCircleAnimationIncrement,
+      finalBottomHeight: this.bottomCircleMaxOpeningHeight,
+      buttonRadiusYIncrement: -this.buttonRadiusYIncrement,
+      finalButtonRadius: this.buttonMinRadiusY,
+      buttonHeightIncrement: -this.buttonHeightIncrement,
+      finalButtonHeight: this.buttonMaxOpeningHeight,
+      clearAnimationCondition: () => this._topOpeningHeight <= this.topCircleMaxOpeningHeight
+    });
+  }
+
+  animateClose(): void {
+    this.animateChangeInOpeningHeight({
+      topHeightIncrement: this.topCircleAnimationIncrement,
+      finalTopHeight: this.circleBaselineOpeningHeight,
+      bottomHeightIncrement: -this.bottomCircleAnimationIncrement,
+      finalBottomHeight: this.circleBaselineOpeningHeight,
+      buttonRadiusYIncrement: this.buttonRadiusYIncrement,
+      finalButtonRadius: this.buttonBaselineRadiusY,
+      buttonHeightIncrement: this.buttonHeightIncrement,
+      finalButtonHeight: this.circleBaselineOpeningHeight,
+      clearAnimationCondition: () => this._topOpeningHeight >= this.circleBaselineOpeningHeight
+    });
+  }
+
+  animateChangeInOpeningHeight(params: {
+    topHeightIncrement: number,
+    finalTopHeight: number
+    bottomHeightIncrement: number,
+    finalBottomHeight: number,
+    buttonRadiusYIncrement: number,
+    finalButtonRadius: number,
+    buttonHeightIncrement: number,
+    finalButtonHeight: number,
+    clearAnimationCondition: () => boolean
+  }): void {
+    this.animatingOpenOrClose = true;
+    const openPokeballInterval = setInterval(() => {
+      if (params.clearAnimationCondition()) {
+        clearInterval(openPokeballInterval);
+        this._topOpeningHeight = params.finalTopHeight;
+        this._bottomOpeningHeight = params.finalBottomHeight;
+        this._buttonRadiusY = params.finalButtonRadius;
+        this._buttonHeight = params.finalButtonHeight;
+        this.animatingOpenOrClose = false;
+      } else {
+        this._topOpeningHeight += params.topHeightIncrement;
+        this._bottomOpeningHeight += params.bottomHeightIncrement;
+        this._buttonRadiusY += params.buttonRadiusYIncrement;
+        this._buttonHeight += params.buttonHeightIncrement;
+      }
+    }, this.animationFrameRate);
+  }
+
+  private get isOpen(): boolean {
+    return this._topOpeningHeight === this.topCircleMaxOpeningHeight;
+  }
+
+  private get circleBaselineOpeningHeight(): number {
+    return 60;
+  }
+
+  private get buttonBaselineRadiusY(): number {
+    return 12.5;
+  }
+
+  private get topCircleMaxOpeningHeight(): number {
+    return 9.975;
+  }
+
+  private get bottomCircleMaxOpeningHeight(): number {
+    return 94.8;
+  }
+
+  private get buttonMaxOpeningHeight(): number {
+    return 24.33;
+  }
+
+  private get buttonMinRadiusY(): number {
+    return 8.585;
+  }
+
+  private get animationFrameRate(): number {
+    return 1000 / 60;
+  }
+
+  private get topCircleAnimationIncrement(): number {
+    return (this.circleBaselineOpeningHeight - this.topCircleMaxOpeningHeight) / 30;
+  }
+
+  private get bottomCircleAnimationIncrement(): number {
+    return (this.bottomCircleMaxOpeningHeight - this.circleBaselineOpeningHeight) / 30;
+  }
+
+  private get buttonRadiusYIncrement(): number {
+    return (this.buttonBaselineRadiusY - this.buttonMinRadiusY) / 30;
+  }
+
+  private get buttonHeightIncrement(): number {
+    return (this.circleBaselineOpeningHeight - this.buttonMaxOpeningHeight) / 30;
+  }
+}
