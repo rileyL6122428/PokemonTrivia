5b4b1b6f5a60c468ac0258d26ae54c562ab9b598
Riley Littlefield <rileylittlefield@ymail.com>
2018-08-25 17:27:57 -0400

    begins answer-selection-animations

--- poketrivia-client/src/app/reusable-ui/answer-selection/answer-selection.component.ts
+++ poketrivia-client/src/app/reusable-ui/answer-selection/answer-selection.component.ts
@@ -1,13 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { Game } from '../../game/game.model';
+import { Pokemon } from '../../pokemon/pokemon.model';
 
 @Component({
   selector: 'pkt-answer-selection',
   templateUrl: './answer-selection.component.html',
-  styleUrls: ['./answer-selection.component.scss']
+  styleUrls: ['./answer-selection.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AnswerSelectionComponent {
 
   @Input() game: Game;
+  selectedPokemon: Pokemon;
+
+  get pokemonChosen(): boolean {
+    return !!this.selectedPokemon;
+  }
+
+  set answer(pokemon: Pokemon) {
+    if (!this.selectedPokemon) {
+      this.selectedPokemon = pokemon;
+    }
+  }
 
 }
