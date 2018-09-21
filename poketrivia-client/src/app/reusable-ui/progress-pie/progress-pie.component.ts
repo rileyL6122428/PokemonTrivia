074d0bc82fee27f07380d46071f7112378e3bdda
Riley Littlefield <rileylittlefield@ymail.com>
2018-09-21 00:38:55 -0400

    begins refactor of progress pie

--- poketrivia-client/src/app/reusable-ui/progress-pie/progress-pie.component.ts
+++ poketrivia-client/src/app/reusable-ui/progress-pie/progress-pie.component.ts
@@ -13,16 +13,37 @@ export class ProgressPieComponent implements OnInit {
     // this.ctx.strokeStyle('red');
     // this.ctx.strokeText('TESTING', 100, 100);
     this.ctx.beginPath();
-    this.ctx.arc(100, 100, 100, - Math.PI / 2, 3 * Math.PI / 2);
+    this.ctx.arc(this.centerX, this.centerY, this.radius, - Math.PI / 2, 3 * Math.PI / 2);
     this.ctx.stroke();
 
+    const angle = 0;
+    const xArcProgressPoint = this.centerX + Math.cos(angle) * this.radius;
+    const yArcProgressPoint = this.centerY + Math.sin(angle) * this.radius;
+    const absStartAngle = -0.5 * Math.PI;
+    const absEndAngle = 1.5 * Math.PI;
+
     this.ctx.beginPath();
-    this.ctx.arc(100, 100, 100, 0, 3 * Math.PI / 2);
+    this.ctx.moveTo(this.centerX, this.centerY - this.radius);
+    this.ctx.lineTo(this.centerX, this.centerY);
+    this.ctx.lineTo(xArcProgressPoint, yArcProgressPoint);
+    this.ctx.arc(this.centerX, this.centerY, this.radius, angle, absEndAngle, false);
     this.ctx.fill();
   }
 
   private get ctx(): CanvasRenderingContext2D {
     return this.canvasRef.nativeElement.getContext('2d');
+  }
+
+  private get centerX(): number {
+    return 100;
+  }
+
+  private get centerY(): number {
+    return 100;
+  }
+
+  private get radius(): number {
+    return 100;
   }
 
 }
