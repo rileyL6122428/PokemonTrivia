c5c0ba73ba1d4949ef3f0c0db5cb2cce9bfb7bd8
Riley Littlefield <rileylittlefield@ymail.com>
2018-10-01 00:04:08 -0400

    deletes dead code

--- src/main/java/org/l2k/trivia2/domain/game/Game.java
+++ src/main/java/org/l2k/trivia2/domain/game/Game.java
@@ -14,29 +14,6 @@ import org.l2k.trivia2.service.QuestionService;
 
 public class Game {
 	
-//	enum Phase {
-//		NOT_STARTED("NOT_STARTED"),
-//		STARTED("STARTED"),
-//		ASKING_QUESTION("ASKING_QUESTION"),
-//		REVEALING_ANSWER("REVEALING_ANSWER"),
-//		STAGING_NEXT_QUESTION("STAGING_NEXT_QUESTION"),
-//		ANNOUNCING_WINNERS("ANNOUNCING_WINNERS");
-//	 
-//	    private String stringRep;
-//	 
-//	    private Phase(String toString) {
-//	        this.stringRep = toString;
-//	    }
-//	    
-//	    public String getAsString() {
-//	    		return this.stringRep; 
-//	    }
-//	}
-	
-	static class PhaseSetting {
-		private int durationMilliseconds;
-	}
-	
 	private Phase phase;
 	private Map<String, Integer> playerNamesToScores;
 	private String roomName;
